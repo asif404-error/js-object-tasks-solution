cd89c3f437141c8a1a2fe8e54f340845f47b82f5
PhantomOblivion <smasif404@gmail.com>
2026-02-04 04:18:20 +0600

    Solved the object related task-4.

---
+++ task-4.js
@@ -0,0 +1,11 @@
+// Task-4: Count the number of properties.
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const keys = Object.keys(student);
+console.log (keys.length);
